197588e1fe48db6db3f9ac75fd1e3634619a9a0a
pkoppineni <pkoppineni@csod.com>
2023-12-20 11:25:24 +0530

    Added two pipelines

---
+++ src/Pipeline.Shell/BackgroundServices/EmployeeBackgroundService.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.Hosting;
+using Pipeline.Framework;
+
+namespace Pipeline.Shell.Pipelines
+{
+    public class EmployeeBackgroundService : BackgroundService
+    {
+        private const string Name = "Employee.Pipeline";
+        private readonly IPipeline _pipeline;
+
+        public EmployeeBackgroundService()
+        {
+            _pipeline = BuildPipeline();
+        }
+
+        private IPipeline BuildPipeline()
+        {
+            return new Framework.Pipeline.Builder()
+                .Name(Name)
+                .AddStep(DomainStepOne, 100, 1000)
+                .AddStep(DomainStepTwo, 10, 2000)
+                .AddStep(DomainStepThree, 200, 3000)
+                .AddStep(DomainStepFour, 60, 4000)
+                .Build();
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (true)
+            {
+                var messages = new List<Message>();
+                var count = 100_00_00;
+                while (count > 0)
+                {
+                    var message = new Message();
+                    messages.Add(message);
+                    count--;
+                }
+                await _pipeline.ProcessMessagesAsync(messages);
+            }
+        }
+
+        private Task<IList<Message>> DomainStepOne(IList<Message> messages)
+        {
+            Console.WriteLine(nameof(DomainStepOne));
+            return Task.FromResult(messages);
+        }
+
+        private Task<IList<Message>> DomainStepTwo(IList<Message> messages)
+        {
+            Console.WriteLine(nameof(DomainStepTwo));
+            return Task.FromResult(messages);
+        }
+
+        private Task<IList<Message>> DomainStepThree(IList<Message> messages)
+        {
+            Console.WriteLine(nameof(DomainStepThree));
+            return Task.FromResult(messages);
+        }
+
+        private Task<IList<Message>> DomainStepFour(IList<Message> messages)
+        {
+            Console.WriteLine(nameof(DomainStepFour));
+            return Task.FromResult(messages);
+        }
+    }
+}
--- src/Pipeline.Shell/Program.cs
+++ src/Pipeline.Shell/Program.cs
@@ -1,32 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Pipeline.Framework;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Pipeline.Shell.Pipelines;
 
-Console.WriteLine("Hello, World!");
 
-Task<IList<Message>> DomainStepOne(IList<Message> messages)
-{
-    Console.WriteLine("Domain Step One");
-    return Task.FromResult(messages);
-}
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<EmployeeBackgroundService>()
+    .AddHostedService<LeaveRequestBackgroundService>();
 
-Task<IList<Message>> DomainStepTwo(IList<Message> messages)
-{
-    Console.WriteLine("Domain Step Two");
-    return Task.FromResult(messages);
-}
-
-var pipeline = new Pipeline.Framework.Pipeline.Builder()
-    .Name("Employee Pipeline")
-    .AddStep(DomainStepOne, 20, 5000)
-    .AddStep(DomainStepTwo, 30, 10000)
-    .Build();
-
-var messages = new List<Message>();
-var count = 100000;
-while (count > 0)
-{
-    messages.Add(new Message());
-    count--;
-}
-await pipeline.ProcessMessagesAsync(messages);
-Console.WriteLine("Complete");
+IHost host = builder.Build();
+host.Run();
